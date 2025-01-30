1d11f083a1715df79eef408c8fd59d6f3deccf10
Laurie Tardif <laurie.tardif@gmail.com>
2025-01-29 20:31:52 -0500

    modified:   src/client/app/list.js

--- src/client/app/list.js
+++ src/client/app/list.js
@@ -1,4 +1,4 @@
-import { getAnimals, deleteAnimal } from "./animals/animal.service.js";
+import { getAnimals, deleteAnimal } from "inft-2202/src/client/app/animals/animal.service.js";
 
 // Function to draw the table with animals
 function drawAnimalsTable(animals) {
