bdfaef8dd4ac02c1d89f85fb482c09f9869ead08
Laurie Tardif <laurie.tardif@gmail.com>
2025-02-02 22:06:16 -0500

    new file:   Assignment1/index.js

---
+++ Assignment1/index.js
@@ -0,0 +1,37 @@
+import '@fortawesome/fontawesome-free';
+import 'bootstrap';
+import Navigo from 'navigo';
+import './img/bear.jpg';
+import './scss/styles.scss';
+
+import FooterComponent from './components/footer/footer';
+import HeaderComponent from './components/header/header';
+import AboutComponent from './pages/about/about';
+import ContactComponent from './pages/contact/contact';
+import ListAnimalComponent from './pages/create-animal/create-animal';
+import HomeComponent from './pages/home/home';
+import CreateAnimalComponent from './pages/list-animals/list-animals';
+
+console.log('Hello World');
+
+export const router = new Navigo('/');
+
+window.addEventListener('load', () =>{
+    HeaderComponent();
+    FooterComponent();
+
+    router
+        .on('/', HomeComponent)
+        .on('/', AboutComponent)
+        .on('/', ContactComponent)
+        .on('/', CreateAnimalComponent)
+        .on('/', ListAnimalComponent)
+        .resolve();
+});
+
+document.addEventListener('click', event => {
+    if (event.target.attributes['route']){
+        event.preventDefault();
+        router.navigate(event.target.attributes['route'].value);
+    }
+});
