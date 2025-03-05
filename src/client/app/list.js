a790768240da6264fc0a72aac32cc75f56c05c05
Laurie Tardif <laurie.tardif@gmail.com>
2025-03-05 14:51:14 -0500

    modified:   src/client/app/list.js

--- src/client/app/list.js
+++ src/client/app/list.js
@@ -1,4 +1,4 @@
-import { getAnimals, deleteAnimal } from './animals/animal.service.js';
+import { getAnimals, getAnimalPage, deleteAnimal } from './animals/animal.service.js';
 
 // Global variable to store the animalId to delete
 let animalNameToDelete = null; //changed the name
