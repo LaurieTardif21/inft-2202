9d75127d76b63c6ef8ed1fa2b73cfb7914bdc50f
Laurie Tardif <laurie.tardif@gmail.com>
2025-03-08 14:54:28 -0500

    modified:   Assignment1/product.js

--- Assignment1/product.js
+++ Assignment1/product.js
@@ -149,11 +149,13 @@ if (productId) {
             return;
         }
 
+        //changed here
         const product = {
             name: name,
             description: description,
             stock: parseInt(stock),
             price: parseFloat(price),
+            id: `${name}-${Date.now()}` // ADDED: Set the composite ID
         };
 
         try {
