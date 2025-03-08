bb6330471817b9033a5fb6b20a25798778bf85e5
Laurie Tardif <laurie.tardif@gmail.com>
2025-03-08 13:09:26 -0500

    modified:   Assignment1/product.js

--- Assignment1/product.js
+++ Assignment1/product.js
@@ -1,5 +1,5 @@
 //import section
-import { getProducts, deleteProduct } from './product.service.js';
+import { getProductPage, deleteProduct } from './product.service.js';
 
 // const section
 const productsList = document.getElementById('products-list');
@@ -16,7 +16,8 @@ const paginationContainer = document.getElementById('paginationContainer');
 
 // pagination
 let currentPage = 1;
-const productsPerPage = 8;
+let perPage = 8;// added this
+const productsPerPage = 8; // keep this for now
 
 // show message box function
 function showMessageBox(message) {
@@ -227,48 +228,40 @@ async function loadProducts(page) {
     showLoadingMessage();
     showLoadingPagination();
     try {
-        const response = await getProducts();
-        console.log("Raw response from getProducts:", response); // Log the raw response
+        const response = await getProductPage(page, perPage);
+        console.log("Raw response from getProductPage:", response); // Log the raw response
         console.log("Type of response:", typeof response); // Log the type
         console.log("Is response an array?", Array.isArray(response)); // Check if it's an array
-
-        if (response) {
-            const startIndex = (page - 1) * productsPerPage;
-            const endIndex = startIndex + productsPerPage;
-
-            //only apply slice if response is an array
-            if (Array.isArray(response)){
-                const paginatedProducts = response.slice(startIndex, endIndex);
-                console.log("paginatedProducts:", paginatedProducts)
-                 if (response.length === 0) {
-                    showMessageBox('No products in the list. Add some products!');
-                    loadingMessageBox.classList.add('d-none');
-                    loadingPaginationMessageBox.classList.add('d-none');
-                    paginationContainer.classList.add('d-none');
-                } else {
-                    const totalPages = Math.ceil(response.length / productsPerPage);
-                    if (totalPages === 1) {
-                        paginationContainer.classList.add('d-none');
-                    } else {
-                        paginationContainer.classList.remove('d-none');
-                    }
-                    hideMessages();
-                    productsList.innerHTML = '';
-                    paginatedProducts.forEach(product => {
-                        const card = createProductCard(product);
-                        productsList.appendChild(card);
-                    });
-                    productsList.classList.remove('d-none');
-
-                    updatePagination(totalPages, page);
-                    enableTooltips();
-                }
-            }
-        } else {
+        if (!response) {
             displayNoService();
+            return;
         }
+       if (response.records.length === 0) {
+            showMessageBox('No products in the list. Add some products!');
+            loadingMessageBox.classList.add('d-none');
+            loadingPaginationMessageBox.classList.add('d-none');
+            paginationContainer.classList.add('d-none');
+        } else {
+            const totalPages = response.pagination.pages;
+            if (totalPages === 1) {
+                paginationContainer.classList.add('d-none');
+            } else {
+                paginationContainer.classList.remove('d-none');
+            }
+            hideMessages();
+            productsList.innerHTML = ''; // Clear existing products
+            response.records.forEach(product => {
+                const card = createProductCard(product);
+                productsList.appendChild(card);
+            });
+            productsList.classList.remove('d-none');
+
+            updatePagination(totalPages, page);
+            enableTooltips();
+        }
+
     } catch (error) {
-         console.log("Error from loadProduct:", error) 
+        console.log("Error from loadProduct:", error)
         displayError(`Error loading products: ${error.message}`);
     }
 }
