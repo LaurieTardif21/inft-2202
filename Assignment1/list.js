150394b5681a7aa4395ed001f5695ca09d5cfc4d
Laurie Tardif <laurie.tardif@gmail.com>
2025-03-09 22:38:51 -0400

    modified:   Assignment1/list.js

--- Assignment1/list.js
+++ Assignment1/list.js
@@ -15,7 +15,7 @@ const paginationContainer = document.getElementById('paginationContainer');
 
 // pagination
 let currentPage = 1;
-const productsPerPage = 5;
+const productsPerPage = 15;
 
 // show message box function
 function showMessageBox(message) {
