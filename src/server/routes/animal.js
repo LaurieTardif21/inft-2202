63d760d32307927e4b764b2c9390b61d12a36fa5
Laurie Tardif <laurie.tardif@gmail.com>
2025-03-17 09:32:51 -0400

    new file:   src/server/errors/ConflictError.js 	modified:   src/server/middleware/validation.js 	modified:   src/server/routes/animal.js

--- src/server/routes/animal.js
+++ src/server/routes/animal.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import animal from "../controllers/animal.js";
+import { checkValidation } from '../middleware/validation.js';
 
 const router = express.Router();
 router.get('/:name?', animal.index);
