6133058a9a59ac62c10296f2b50eade51a3deee8
Laurie Tardif <laurie.tardif@gmail.com>
2025-03-18 12:20:06 -0400

    new file:   Assignment2/server/service/config.js 	new file:   Assignment2/server/service/dataService.js 	new file:   Assignment2/server/service/index.js

---
+++ Assignment2/server/service/config.js
@@ -0,0 +1,30 @@
+import bodyParser from 'body-parser';
+import productRouter from '../routes/product.js'
+import { loggingMiddleware } from '../middleware/logging.js';
+import {errorHandler} from '../middleware/errorhandler.js';
+import { query,validationResult } from 'express-validator';
+
+function config(app) {
+    
+    // Parse JSON bodies and URL-encoded bodies
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    
+    app.use(loggingMiddleware);
+
+    app.get("/hello", query('person').notEmpty(),(req, res) => {
+        const result = validationResult(req);
+        if (result.isEmpty()) {
+          let helloContent = `<!DOCTYPE html><html lang=\"en-us\"><head><title>INFT 2202</title></head><body><main><h1>Hello ${req.query.person} from Express</h1><p>at ${new Date()}</p></main></body></html>`;
+          res.send(helloContent);
+        }
+        res.send({ errors: result.array() })
+    }); 
+        
+    app.use('/api/products', productRouter);
+
+// Error handler middleware (should be the last middleware)
+    app.use(errorHandler);      
+}
+
+export default config;
