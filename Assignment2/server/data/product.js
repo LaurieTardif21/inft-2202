f6e311c43b4e60c69b32fec2d198b0b9731c9d0a
Laurie Tardif <laurie.tardif@gmail.com>
2025-03-18 12:14:18 -0400

    new file:   Assignment2/server/data/product.js 	new file:   Assignment2/server/data/product.json

---
+++ Assignment2/server/data/product.js
@@ -0,0 +1,183 @@
+export default [
+    {
+      "name": "Gorge",
+      "description": "Grizzly Bear",
+      "stock": 4,
+      "price": 4
+    },
+    {
+      "name": "name 0",
+      "description": "Grizzly Bear",
+      "stock": 4,
+      "price": 2,
+      "sound": "Moo"
+    },
+    {
+      "name": "name 1",
+      "description": "Grizzly Bear",
+      "stock": 4,
+      "price": 2,
+      "sound": "Moo"
+    },
+    {
+      "name": "name 2",
+      "description": "Grizzly Bear",
+      "stock": 4,
+      "price": 2,
+      "sound": "Moo"
+    },
+    {
+      "name": "name 3",
+      "description": "Grizzly Bear",
+      "stock": 4,
+      "price": 2,
+      "sound": "Moo"
+    },
+    {
+      "name": "name 4",
+      "description": "Grizzly Bear",
+      "stock": 4,
+      "price": 2,
+      "sound": "Moo"
+    },
+    {
+      "name": "name 5",
+      "description": "Grizzly Bear",
+      "stock": 4,
+      "price": 2,
+      "sound": "Moo"
+    },
+    {
+      "name": "name 6",
+      "description": "Grizzly Bear",
+      "stock": 4,
+      "price": 2,
+      "sound": "Moo"
+    },
+    {
+      "name": "name 7",
+      "description": "Grizzly Bear",
+      "stock": 4,
+      "price": 2,
+      "sound": "Moo"
+    },
+    {
+      "name": "name 8",
+      "description": "Grizzly Bear",
+      "stock": 4,
+      "price": 2,
+      "sound": "Moo"
+    },
+    {
+      "name": "name 9",
+      "description": "Grizzly Bear",
+      "stock": 4,
+      "price": 2,
+      "sound": "Moo"
+    },
+    {
+      "name": "name 10",
+      "description": "Grizzly Bear",
+      "stock": 4,
+      "price": 2,
+      "sound": "Moo"
+    },
+    {
+      "name": "name 11",
+      "description": "Grizzly Bear",
+      "stock": 4,
+      "price": 2,
+      "sound": "Moo"
+    },
+    {
+      "name": "name 12",
+      "description": "Grizzly Bear",
+      "stock": 4,
+      "price": 2,
+      "sound": "Moo"
+    },
+    {
+      "name": "name 13",
+      "description": "Grizzly Bear",
+      "stock": 4,
+      "price": 2,
+      "sound": "Moo"
+    },
+    {
+      "name": "name 14",
+      "description": "Grizzly Bear",
+      "stock": 4,
+      "price": 2,
+      "sound": "Moo"
+    },
+    {
+      "name": "name 15",
+      "description": "Grizzly Bear",
+      "stock": 4,
+      "price": 2,
+      "sound": "Moo"
+    },
+    {
+      "name": "name 16",
+      "description": "Grizzly Bear",
+      "stock": 4,
+      "price": 2,
+      "sound": "Moo"
+    },
+    {
+      "name": "name 17",
+      "description": "Grizzly Bear",
+      "stock": 4,
+      "price": 2,
+      "sound": "Moo"
+    },
+    {
+      "name": "name 18",
+      "description": "Grizzly Bear",
+      "stock": 4,
+      "price": 2,
+      "sound": "Moo"
+    },
+    {
+      "name": "name 19",
+      "description": "Grizzly Bear",
+      "stock": 4,
+      "price": 2,
+      "sound": "Moo"
+    },
+    {
+      "name": "name 20",
+      "description": "Grizzly Bear",
+      "stock": 4,
+      "price": 2,
+      "sound": "Moo"
+    },
+    {
+      "name": "name 21",
+      "description": "Grizzly Bear",
+      "stock": 4,
+      "price": 2,
+      "sound": "Moo"
+    },
+    {
+      "name": "name 22",
+      "description": "Grizzly Bear",
+      "stock": 4,
+      "price": 2,
+      "sound": "Moo"
+    },
+    {
+      "name": "name 23",
+      "description": "Grizzly Bear",
+      "stock": 4,
+      "price": 2,
+      "sound": "Moo"
+    },
+    {
+      "name": "name 24",
+      "description": "Grizzly Bear",
+      "stock": 4,
+      "price": 2,
+      "sound": "Moo"
+    }
+  ];
