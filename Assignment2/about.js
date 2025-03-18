8e84529b849f1d4f1cf24b68be87d575181a38fd
Laurie Tardif <laurie.tardif@gmail.com>
2025-03-18 11:35:15 -0400

    deleted:    Assignment2/about.html 	deleted:    Assignment2/about.js 	deleted:    Assignment2/contact.html 	deleted:    Assignment2/contact.js 	deleted:    Assignment2/create.html 	deleted:    Assignment2/create.js 	deleted:    Assignment2/index.html 	deleted:    Assignment2/index.js 	deleted:    Assignment2/list.html 	deleted:    Assignment2/list.js 	deleted:    Assignment2/product.html 	deleted:    Assignment2/product.js 	deleted:    Assignment2/product.service.js 	deleted:    Assignment2/search.html 	deleted:    Assignment2/search.js 	deleted:    Assignment2/style.css

--- Assignment2/about.js
+++
@@ -1,5 +0,0 @@
-// Name: Laurie Tardif
-// Date: 02/08/2025
-// Course Code: INFT 2202
-// Section Number: 05
-// Description of file: About js explaining who I am
--- Assignment2/contact.js
+++
@@ -1,6 +0,0 @@
-// contact validation here
-
-//name: laurie tardif
-//description: contact functions
-// course: inft 2202
-// date: 02/09/2025
