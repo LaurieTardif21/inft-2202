0dc5c4e2fa825698db1d8ae91dfb8c33e0e101b5
Laurie Tardif <laurie.tardif@gmail.com>
2025-03-18 12:16:01 -0400

    new file:   Assignment2/server/routes/product.js

---
+++ Assignment2/server/routes/product.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import product from "../controllers/product.js";
+import { checkValidation } from '../middleware/validation.js';
+
+const router = express.Router();
+router.get('/:name?', prpduct.index);
+router.post('/', checkValidation(product.rules), product.add);
+router.delete('/:name?', product.delete);
+router.put('/', product.update);
+
+export default router;
