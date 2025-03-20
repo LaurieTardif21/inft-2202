c068d857cf46d9f72fab426213fea97051d031fb
Laurie Tardif <laurie.tardif@gmail.com>
2025-03-20 13:02:42 -0400

    modified:   Assignment2/server/routes/product.js 	modified:   package-lock.json 	modified:   package.json

--- Assignment2/server/routes/product.js
+++ Assignment2/server/routes/product.js
@@ -3,7 +3,7 @@ import product from "../controllers/product.js";
 import { checkValidation } from '../middleware/validation.js';
 
 const router = express.Router();
-router.get('/:name?', prpduct.index);
+router.get('/:name?', product.index);
 router.post('/', checkValidation(product.rules), product.add);
 router.delete('/:name?', product.delete);
 router.put('/', product.update);
