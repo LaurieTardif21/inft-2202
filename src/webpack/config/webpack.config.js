7d717a5e09224453ce41099e36dfa10814f8eb68
Laurie Tardif <laurie.tardif@gmail.com>
2025-03-26 15:31:46 -0400

    new file:   src/webpack/config/public/index.html 	modified:   src/webpack/config/webpack.config.js

--- src/webpack/config/webpack.config.js
+++ src/webpack/config/webpack.config.js
@@ -7,48 +7,42 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-  context: (__dirname, '../../'), // Base directory for resolving entry points and loaders
   devtool: 'source-map',
-  entry: './src/webpack/index.js', // Entry point relative to the context (current directory)
+  entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'), // Output to the 'dist' folder in the current directory
+    path: path.resolve(__dirname, '../dist'),
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html', // Path to your HTML file within the current directory
+      template: './public/index.html',
       inject: 'body', // Injects the script tag at the end of the body
     }),
     new CopyWebpackPlugin({
       patterns: [
-        { from: './img', to: 'img' } // Path to your image directory
+        { from: './public/img', to: 'img' }//it will copy to a temp folder under dev mode
       ]
     })
-  ],
+  ],  
   devServer: {
     static: {
-      directory: path.join(__dirname, 'public'), // Serve content from the public directory
+      directory: path.join(__dirname, 'public'),
     },
     compress: true,
     port: 9000,
     open: true,
   },
-  resolve: {
-    fallback: {
-      fs: false,
-      path: require.resolve('path-browserify'),
-      timers: require.resolve('timers-browserify')
-    }
-  },
   module: {
     rules: [
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
-        type: 'asset/resource', // Using Webpack 5 native asset module
-        generator: {
-          filename: 'img/[name].[contenthash][ext][query]', // Adjust image output path
+        loader: 'file-loader',
+        options: {
+          esModule: false,
+          name: '[name].[contenthash].[ext]',
+          outputPath: 'img',
         },
-      },
+      },      
       {
         test: /\.ejs$/,
         loader: 'ejs-loader',
