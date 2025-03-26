24ee52efe1462e0085e9263bfb28a4ba76d1556a
Laurie Tardif <laurie.tardif@gmail.com>
2025-03-26 12:16:32 -0400

    new file:   src/webpack/public/add.html 	new file:   src/webpack/public/add.js 	new file:   src/webpack/public/animal.html 	new file:   src/webpack/public/animal.js 	new file:   src/webpack/public/animal.service.js 	new file:   src/webpack/public/contact.html 	new file:   src/webpack/public/fetch.js 	new file:   src/webpack/public/index.html 	new file:   src/webpack/public/index.js 	new file:   src/webpack/public/list.html 	new file:   src/webpack/public/list.js

---
+++ src/webpack/config/public/fetch.js
@@ -0,0 +1,88 @@
+const serverUrl = 'https://inft2202-server.onrender.com';
+const dcUrl = 'https://inft2202.opentech.durhamcollege.org';
+const localUrl = 'http://localhost:3091';
+
+
+const _url = dcUrl;
+function list(){
+
+  const url = new URL('/api/animals', _url);
+  const options = {
+    method: 'GET', // or 'POST', 'PUT', etc.
+    headers: {
+      //'Content-Type': 'application/json',
+      //'User': 'studentId',
+      'apiKey': '06ecdba4-4ac1-40bd-83a8-b74a04430a49'
+    }
+  };
+
+  fetch(url, options)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data); // Process the data here
+  })
+  .catch(err=>{
+    console.error('Error fetching data:', err);
+  });
+}
+
+async function load(){
+  const url = new URL('/api/animals', _url);
+  const options = {
+    method: 'GET', // or 'POST', 'PUT', etc.
+    headers: {
+      //'Content-Type': 'application/json',
+      //'User': 'studentId',
+      'apiKey': '06ecdba4-4ac1-40bd-83a8-b74a04430a49'
+    }
+  };
+  
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+/*
+async function add(){
+  const url = new URL('/api/animals', _url);
+  const options = {
+    method: 'POST', // or 'POST', 'PUT', etc.
+    headers: {
+      'Content-Type': 'application/json',
+      'user': 'studentId'
+    },
+    body: JSON.stringify([{
+      "name": "Gorge1",
+      "breed": "Grizzly Bear",
+      "legs": 4,
+      "eyes": 4,
+      "sound": "Moo"        
+    }])
+  };
+  
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}*/
+
+//await add();
+//await load();
+list();
