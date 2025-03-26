4ae992746d53a61a5a35e8643331af1ab39d289b
Laurie Tardif <laurie.tardif@gmail.com>
2025-03-26 15:18:27 -0400

    modified:   src/webpack/config/webpack.config.js

--- src/webpack/config/webpack.config.js
+++ src/webpack/config/webpack.config.js
@@ -7,27 +7,27 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-  context: path.resolve(__dirname, '../../src'),
+  context: path.resolve(__dirname, '../../src'), // Adjust context to the 'src' folder
   devtool: 'source-map',
-  entry: './index.js',
+  entry: './index.js', // Entry point relative to the context (src folder)
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, '../../../dist'),
+    path: path.resolve(__dirname, '../../../dist'), // Output to the 'dist' folder in the project root
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html',
-      inject: 'body',
+      template: './index.html', // Adjust the path to your HTML file within 'src'
+      inject: 'body', // Injects the script tag at the end of the body
     }),
     new CopyWebpackPlugin({
       patterns: [
-        { from: './img', to: 'img' }
+        { from: './img', to: 'img' } // Adjust the path to your image directory
       ]
     })
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, '../../public'),
+      directory: path.join(__dirname, '../../src'), // Serve content from the src directory
     },
     compress: true,
     port: 9000,
@@ -36,17 +36,17 @@ export default {
   resolve: {
     fallback: {
       fs: false,
-      path: require.resolve('path-browserify'), // Polyfill for path module
-      timers: require.resolve('timers-browserify'), // Polyfill for timers module
+      path: require.resolve('path-browserify'),
+      timers: require.resolve('timers-browserify')
     }
   },
   module: {
     rules: [
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
-        type: 'asset/resource',
+        type: 'asset/resource', // Using Webpack 5 native asset module
         generator: {
-          filename: 'img/[name].[contenthash][ext][query]',
+          filename: 'img/[name].[contenthash][ext][query]', // Adjust image output path
         },
       },
       {
