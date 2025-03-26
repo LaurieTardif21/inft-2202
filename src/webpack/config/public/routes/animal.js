215c0974f70975fac92055cbc65acacc4a0f8aee
Laurie Tardif <laurie.tardif@gmail.com>
2025-03-26 13:56:14 -0400

    new file:   src/webpack/config/public/routes/animal.js

---
+++ src/webpack/config/public/routes/animal.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import animal from "./public/controllers/animal.js";
+import { checkValidation } from './public/middleware/validation.js';
+
+const router = express.Router();
+router.get('/:name?', animal.index);
+router.post('/', checkValidation(animal.rules), animal.add);
+router.delete('/:name?', animal.delete);
+router.put('/', animal.update);
+
+export default router;
