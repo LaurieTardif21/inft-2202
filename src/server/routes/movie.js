938c37829444f57fe9878d0cd1d89ba82adae428
Laurie Tardif <laurie.tardif@gmail.com>
2025-03-28 13:46:29 -0400

    deleted:    src/server/controllers/movie.js 	deleted:    src/server/data/movies.js 	deleted:    src/server/movieindex.js 	deleted:    src/server/public/movieindex.html 	deleted:    src/server/routes/movie.js 	modified:   src/server/service/config.js

--- src/server/routes/movie.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import movie from "../controllers/movie.js";
-import { checkValidation } from '../middleware/validation.js';
-
-const router = express.Router();
-router.get('/:title?', movie.index);
-router.post('/', checkValidation(movie.rules), movie.add);
-
-export default router;
